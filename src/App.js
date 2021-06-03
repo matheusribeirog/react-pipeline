7433203a1ee0ca78f7ed72a712840fdd5ab21d70
Matheus Ribeiro <matheus.gomes94@etec.sp.gov.br>
2021-06-03 01:10:15 +0000

    teste

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Pipeline demo para a Mara. Teste
+          Pipeline demo para a Mara. Final Success
         </p>
         <a
           className="App-link"
